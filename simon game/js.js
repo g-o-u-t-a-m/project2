769475c2a2c6aac06d3fadc51ec421eca1c46c3f
GOUTAM MANNA <gpg089600@gmail.com>
2026-01-28 22:23:12 +0530

    simon game

---
+++ simon game/js.js
@@ -0,0 +1,68 @@
+let gameSeq=[];
+let userSeq=[];
+let btns=["yellow" , "red" ,"purple" , "green"];
+let started=false;
+let level=0;
+let h2=document.querySelector('h2');
+
+document.addEventListener("keypress",function(){
+    if(started==false){
+        console.log("game is started");
+        started=true;
+        levelUp();
+    }
+});
+
+function btnFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    },250);
+}
+
+function levelUp(){
+    userSeq=[];
+    level++;
+    h2.innerText=`Level ${level}`;
+    let randIdx=Math.floor(Math.random()*3);
+    let randColor=btns[randIdx];
+    let randbtn=document.querySelector(`.${randColor}`);
+    btnFlash(randbtn);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+}
+
+function checkAns(idx){
+    if(userSeq[idx]===gameSeq[idx]){
+        if(userSeq.length==gameSeq.length){
+            setTimeout(levelUp,1000);
+        }
+    }else{
+        
+        h2.innerHTML=`Game Over! your score was <b> ${level} </b> <br>press any key to start`;
+        document.querySelector("body").style.backgroundColor="red";
+        setTimeout(function(){
+             document.querySelector("body").style.backgroundColor="white";
+        },150);
+        reset();
+    }
+}
+
+let allBtns=document.querySelectorAll(".btn");
+for(btn of allBtns){
+    btn.addEventListener("click",function(){
+        let btn=this;
+        btnFlash(btn);
+        userColor=btn.getAttribute("id");
+        userSeq.push(userColor);
+        checkAns(userSeq.length-1);
+    })
+}
+
+function reset(){
+    started=false;
+    gameSeq=[];
+    userSeq=[];
+    level=0;
+}
+
